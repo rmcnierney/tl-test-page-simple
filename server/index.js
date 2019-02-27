1a21bfbd48334c090adb8db0ab3e81a297354e1b
Ryan McNierney <45180751+rmcnierney@users.noreply.github.com>
2019-02-27 15:37:44 -0500

    fixed port

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const morgan = require('morgan')
-const PORT = process.env.PORT | 3000
+const PORT = process.env.PORT || 3000
 
 app.use(morgan('dev')); //logging middleware
 app.use(express.static(path.join(__dirname, '..', 'public'))) // static file-serving middleware
